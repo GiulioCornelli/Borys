2f571fbfc4b2576dc9d8e37e71b5ef4d4939cac3
GiulioCornelli <cornelli.giulio05@gmail.com>
2026-07-30 16:10:11 +0200

    Implementazione del ControllerAgente, con le sue funzionalità

---
+++ borys-ai-agent/src/main/java/com/agents/ControllerAgent.java
@@ -0,0 +1,43 @@
+package com.agents;
+
+
+import com.Tools.NameTool;
+
+import dev.langchain4j.service.SystemMessage;
+import dev.langchain4j.service.UserMessage;
+import io.quarkiverse.langchain4j.RegisterAiService;
+import jakarta.inject.Singleton;
+
+@RegisterAiService(tools = NameTool.class)
+@Singleton
+public interface ControllerAgent {
+
+    enum CategoryCustom {GETVALU, SETVALU, NAME ,GENERAL}
+
+    @SystemMessage("""
+        Sei un agente orchestratore che deve analizzare il testo in ingresso,
+        ed etichettarlo con una delle seguenti etichette:
+        
+        - GETVALU: Se l'utente deve ottenere dei dati da dei dispositivi 
+        
+        - SETVALU: Se l'utente vuole modificare lo stato, un valore o altre caratteristiche di un dispositivo IoT
+
+        - NAME: Se ti chiedono quale è il tuo nome
+        
+        - GENERAL: Se non è nessuna delle altre
+
+        (Mi raccomando rispondi solo con una di queste etichette)
+        
+    """)
+
+    CategoryCustom FindeCategory(@UserMessage String message);
+    String resolvRequest(@UserMessage String message);
+
+
+    @SystemMessage("""
+        Sei un assistente amichevole. Quando ti viene chiesto il tuo nome,
+        usa il tool getName per rispondere.
+    """)
+    String SendYourName(@UserMessage String message);
+
+}
